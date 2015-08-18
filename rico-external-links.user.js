4408bc0845cc7e0388b05c9c9c744b1218c8b380
Humberto Anjos <h.anjos@acm.org>
2015-08-18 20:51:58 -0300

    Os botões funcionam agora na página de ações também.

--- rico-external-links.user.js
+++ rico-external-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         External links for Rico.com.vc
 // @namespace    https://www.rico.com.vc
-// @version      0.2
+// @version      0.3
 // @description  Cria botões que adicionam links para análises de papéis e empresas específicas. Por enquanto são suportadas Fundamentus e Bastter.
 // @author       Humberto S. N. dos Anjos
 // @match        https://www.rico.com.vc/*
@@ -9,10 +9,15 @@
 // ==/UserScript==
 
 // Usando o location hack (http://wiki.greasespot.net/Location_hack) para injetar
-// funções no escopo global a serem acessadas pelos botões criados abaixo
+// funções no escopo global
 location.replace("javascript:(" + function() {
   window.linkToFundamentus = function() {
     $("tr[ng-repeat=\"stock in recommendation.portfolio | tablesortOrderBy:sortFun\"]>td:nth-child(2)")
+    .each(function (v, i) {
+      $(this).html("<a href='http://fundamentus.com.br/detalhes.php?papel=" + $(this).text() + "'>" + $(this).text() + "</a>")
+    });
+
+    $("tbody[ng-repeat=\"currentPosition in customerPosition.positions\"]>tr>td:nth-child(1)")
     .each(function (v, i) {
       $(this).html("<a href='http://fundamentus.com.br/detalhes.php?papel=" + $(this).text() + "'>" + $(this).text() + "</a>")
     });
@@ -23,9 +28,15 @@ location.replace("javascript:(" + function() {
     .each(function (v, i) {
       $(this).html("<a href='http://www.bastter.com/mercado/acao/" + $(this).text() + ".aspx'>" + $(this).text() + "</a>")
     });
+
+    $("tbody[ng-repeat=\"currentPosition in customerPosition.positions\"]>tr>td:nth-child(1)")
+    .each(function (v, i) {
+      $(this).html("<a href='http://www.bastter.com/mercado/acao/" + $(this).text() + ".aspx'>" + $(this).text() + "</a>")
+    });
   };
 } + ")();");
 
+// Estes botões chamarão as funções definidas acima
 var fundamentusButton = "<button onclick='linkToFundamentus()' class='btn-round btn-blue btn-block no-padding-left no-padding-right'>+ Fundamentus</button>";
 var bastterButton = "<button onclick='linkToBastter()' class='btn-round btn-gray-light btn-block no-padding-left no-padding-right'>+ Bastter</button>";
 
